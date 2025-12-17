3a4f6d8af251806dcaba2c812cb2e874859c74d2
youngrok kim <95115004+rogi-rogi@users.noreply.github.com>
2025-12-17 23:56:39 +0900

    프로젝트 초기 생성

---
+++ src/main/java/jojo/jjdc/JjdcApplication.java
@@ -0,0 +1,13 @@
+package jojo.jjdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjdcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jojo/jjdc/JjdcApplicationTests.java
@@ -0,0 +1,13 @@
+package jojo.jjdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjdcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
